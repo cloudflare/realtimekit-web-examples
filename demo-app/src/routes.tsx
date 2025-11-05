90da470ff8fe6adff4e67591f7e1b9af43b16baa
ikabra <ikabra@cloudflare.com>
2025-11-05 16:39:46 +0530

    fix: add an explicit react import in routes

--- demo-app/src/routes.tsx
+++ demo-app/src/routes.tsx
@@ -1,4 +1,4 @@
-// src/routes.tsx
+// @ts-expect-error - NOTE(ikabra): React is needed for JSX transformation in Workers build
 import React from "react";
 import { type RouteObject } from "react-router-dom";
 import Layout from "./layout";
