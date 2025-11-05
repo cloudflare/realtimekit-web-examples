e7c97e7a9446f52d7f2f03c548bfe4c7d2989baa
ikabra <ikabra@cloudflare.com>
2025-11-05 16:59:04 +0530

    fix: types for env variables

--- demo-app/src/vite-env.d.ts
+++ demo-app/src/vite-env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string
   readonly VITE_ORG_ID: string
-  readonly VTIE_BASE_URL: string
+  readonly VITE_BASE_URL: string
 }
 
 interface ImportMeta {
