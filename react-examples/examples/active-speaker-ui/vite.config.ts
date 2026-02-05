00258848f831c949837248e4251c448f79ab109b
ikabra <ikabra@cloudflare.com>
2026-02-05 17:41:37 +0530

    fix: build errors

--- react-examples/examples/active-speaker-ui/vite.config.ts
+++ react-examples/examples/active-speaker-ui/vite.config.ts
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base:'/active-speaker-ui/',
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 6000,
-  },
 });
--- react-examples/examples/chat-widget/src/components/ChatPopup.tsx
+++ react-examples/examples/chat-widget/src/components/ChatPopup.tsx
@@ -1,28 +1,16 @@
 import { RtkChat, RtkMeeting, RtkSpinner } from '@cloudflare/realtimekit-react-ui';
 import { useRealtimeKitClient } from '@cloudflare/realtimekit-react';
-import { ChatChannel } from '@cloudflare/realtimekit';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 type RtkMeeting = Awaited<ReturnType<typeof useRealtimeKitClient>>[0];
 
 const ChatPopup = ({ meeting }: { meeting: RtkMeeting }) => {
-	const [channel, setChannel] = useState<ChatChannel | null>(null);
 
 	useEffect(() => {
 		if (!meeting) return;
-		if (channel) return;
-		const createChannel = async () => {
-			if (!meeting.self.roomJoined) {
-				await meeting.joinRoom();
-			}
-			const memberIds = meeting.participants.all.toArray().map((p) => p.userId);
-			const createdChannel = await meeting.chat.createChannel(meeting.meta.meetingTitle, memberIds);
-			setChannel(createdChannel);
-		};
-		createChannel();
-	}, [channel, meeting]);
+	}, [meeting]);
 
-	if (meeting && channel) {
+	if (meeting) {
 		return (
             <main className="flex h-96 w-96 flex-col rounded-md p-2">
                 <RtkChat meeting={meeting} />
